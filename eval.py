bdf0d0329a7246628569f932d78449361e950702
khangluong2004 <khangluong2004@gmail.com>
2024-07-26 11:59:51 +1000

    Add notes and check

--- eval.py
+++ eval.py
@@ -37,7 +37,7 @@ def calcPL(prcHist):
     (_, nt) = prcHist.shape
     start = 300
 
-    for t in range(750, 1001):
+    for t in range(300, 1001):
         prcHistSoFar = prcHist[:, :t]
         newPosOrig = getPosition(prcHistSoFar)
         curPrices = prcHistSoFar[:, -1]
